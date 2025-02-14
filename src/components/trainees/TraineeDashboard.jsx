c3f9dcfe3cf77ad9d3d144a8b2dd0c642e7dcfc0
kelvin <denlinato@gmail.com>
2025-02-14 10:04:09 +0000

    fix

--- src/components/trainees/TraineeDashboard.jsx
+++ src/components/trainees/TraineeDashboard.jsx
@@ -49,18 +49,31 @@ const TraineeDashboard = () => {
     //     }
     // }, [data, isSuccess]);
  const data = {
-   
-     "Medical Emergency": [
-       {
-         _id: 1,
-         title: "title 1",
-         url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-         description:
-           "A popular open-source animated short film, available in multiple resolutions and bitrates.",
-       },
-     ],
-   
-};
+   "Medical Emergency": [
+     {
+       _id: 1,
+       title: "title 1",
+       url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+       description:
+         "A popular open-source animated short film, available in multiple resolutions and bitrates.",
+     },
+     {
+       title: "Tears of Steel",
+       _id: "2",
+       url: "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+       description:
+         "An open-source science fiction short film, used to test various streaming scenarios.",
+     },
+     {
+       title: "fMP4 HLS Stream",
+       _id: "3",
+       url: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
+       description:
+         "An example of an HLS stream using fragmented MP4 segments, provided by Apple.",
+     },
+    
+   ],
+ };
     return (
   
 
--- src/constant.js
+++ src/constant.js
@@ -8,4 +8,19 @@ export const videoData = [
     description:
       "A popular open-source animated short film, available in multiple resolutions and bitrates.",
   },
+  {
+    title: "Tears of Steel",
+    _id: "2",
+    url: "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+    description:
+      "An open-source science fiction short film, used to test various streaming scenarios.",
+  },
+  {
+    title: "fMP4 HLS Stream",
+    _id: "3",
+    url: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
+    description:
+      "An example of an HLS stream using fragmented MP4 segments, provided by Apple.",
+  },
+  
 ];
